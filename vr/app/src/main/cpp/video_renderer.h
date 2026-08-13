325ca5e1a7d74081e5898ef1096734cffc48c75b
beacondes <m13331262132_1@163.com>
2026-08-13 11:39:04 +0800

    Add video renderer header

---
+++ vr/app/src/main/cpp/video_renderer.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <GLES3/gl3.h>
+#include <openxr/openxr.h>
+
+// Renders a textured quad (OES external texture from SurfaceTexture) in VR space.
+class VideoRenderer {
+public:
+    bool Init();
+    // Draw the video quad for one eye using the view pose + fov.
+    void Render(GLuint oesTexture, const XrPosef& viewPose, const XrFovf& fov);
+    void Shutdown();
+
+private:
+    GLuint program_ = 0;
+    GLuint vao_ = 0;
+    GLuint vbo_ = 0;
+    GLint uMvp_ = -1;
+    GLint uTex_ = -1;
+    bool init_ = false;
+};
